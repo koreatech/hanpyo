1c52257d29bdcaa5db39d95464430c049c83e3a9
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-02-04 22:28:44 +0900

    style: Material UI 테마 정의 및 추가
    - 한표 디자인에 맞게 Material UI 컴포넌트들을 커스텀 하기 위해 테마 정의
    - 하위 컴포넌트에서 테마를 참조하기 위해 최상단 루트에서 ThemeProvider
      Wrapping

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from '@/common/styles';
 import { Router } from './router';
 import apollo from './apollo';
 
 const App = (): JSX.Element => {
   return (
     <ApolloProvider client={apollo}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </ApolloProvider>
   );
 };
