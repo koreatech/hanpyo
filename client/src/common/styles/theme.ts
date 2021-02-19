8e0d6e4f207e6527bfff19e597597f18e1d3dc17
jinhyukoo <faill456@naver.com>
2021-02-20 00:24:35 +0900

    feat: 자주 쓰는 색상 palette에 추가

--- client/src/common/styles/theme.ts
+++ client/src/common/styles/theme.ts
@@ -1,4 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 
 const theme = createMuiTheme({
   overrides: {
@@ -20,6 +21,42 @@ const theme = createMuiTheme({
         '&:hover': {
           backgroundColor: '#f5f5f5',
         },
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: '#ffb84d',
+    },
+    secondary: {
+      main: '#f3f3f3',
+    },
+    grey: {
+      50: grey[50],
+      100: grey[100],
+      200: grey[200],
+      300: grey[300],
+      400: grey[400],
+      500: grey[500],
+      600: grey[600],
+      700: grey[700],
+      800: grey[800],
+      900: grey[900],
+    },
+  },
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: 'h2',
+        h2: 'h2',
+        h3: 'h2',
+        h4: 'h2',
+        h5: 'h2',
+        h6: 'h2',
+        subtitle1: 'h2',
+        subtitle2: 'h2',
+        body1: 'span',
+        body2: 'span',
       },
     },
   },
