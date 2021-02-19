641afead20041895e7edd283e615b27c2042411e
jinhyukoo <faill456@naver.com>
2021-02-20 00:24:51 +0900

    feat: Typography 컴포넌트 제작

---
+++ client/src/components/UI/atoms/Typography/Typography.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+enum TypographyType {
+  primary = 'primary',
+  secondary = 'secondary',
+}
+
+interface TypographyProps {
+  content: string;
+  size: string;
+  typoType: TypographyType;
+  css?: string;
+  onClick?: () => void;
+}
+
+interface StyleProps {
+  size?: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  primary: {
+    color: theme.palette.primary.main,
+  },
+  secondary: {
+    color: theme.palette.secondary.main,
+  },
+}));
+
+const StyledTypography = ({ content, size, typoType, css, onClick }: TypographyProps): JSX.Element => {
+  const classes = useStyles({ size });
+
+  const getClassName = () => {
+    return { ...classes }[typoType];
+  };
+
+  const Sizes: any = {
+    XL: '3.5em',
+    L: '2.2em',
+    M: '1.5em',
+    S: '1em',
+    XS: '0.7em',
+  };
+
+  const getSize = () => {
+    return { ...Sizes }[size];
+  };
+
+  const StyleTypography = styled(Typography)`
+    ${css}
+    font-size: ${getSize()}
+  `;
+
+  return (
+    <StyleTypography className={getClassName()} onClick={onClick}>
+      {content}
+    </StyleTypography>
+  );
+};
+
+export { StyledTypography, TypographyType };
+export type { TypographyProps };
--- client/src/components/UI/atoms/index.ts
+++ client/src/components/UI/atoms/index.ts
@@ -2,3 +2,5 @@ export { Hello } from './Hello/Hello';
 export type { HelloProps } from './Hello/Hello';
 export { StyledButton as Button, ButtonType } from './Button/Button';
 export type { ButtonProps } from './Button/Button';
+export { StyledTypography as Typography, TypographyType } from './Typography/Typography';
+export type { TypographyProps } from './Typography/Typography';
