a89675f316693e2271534552ef17360050fd1929
jinhyukoo <faill456@naver.com>
2021-02-22 14:16:50 +0900

    feat: Typography Type에 색상 추가
    - 회색은 자주 쓰이는 색상이기 때문에 미리 추가 해놓음

--- client/src/components/UI/atoms/Typography/Typography.tsx
+++ client/src/components/UI/atoms/Typography/Typography.tsx
@@ -6,6 +6,17 @@ import styled from 'styled-components';
 enum TypographyType {
   primary = 'primary',
   secondary = 'secondary',
+  secondaryDark = 'secondaryDark',
+  grey1 = 'grey1',
+  grey2 = 'grey2',
+  grey3 = 'grey3',
+  grey4 = 'grey4',
+  grey5 = 'grey5',
+  grey6 = 'grey6',
+  grey7 = 'grey7',
+  grey8 = 'grey8',
+  grey9 = 'grey9',
+  grey0 = 'grey0',
 }
 
 interface TypographyProps {
@@ -22,6 +33,42 @@ const useStyles = makeStyles((theme) => ({
   },
   secondary: {
     color: theme.palette.secondary.main,
+  },
+  secondaryDark: {
+    color: theme.palette.secondary.dark,
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
+  },
+  grey1: {
+    color: theme.palette.grey[100],
+  },
+  grey2: {
+    color: theme.palette.grey[200],
+  },
+  grey3: {
+    color: theme.palette.grey[300],
+  },
+  grey4: {
+    color: theme.palette.grey[400],
+  },
+  grey5: {
+    color: theme.palette.grey[500],
+  },
+  grey6: {
+    color: theme.palette.grey[600],
+  },
+  grey7: {
+    color: theme.palette.grey[700],
+  },
+  grey8: {
+    color: theme.palette.grey[800],
+  },
+  grey9: {
+    color: theme.palette.grey[900],
+  },
+  grey0: {
+    color: theme.palette.grey[50],
   },
 }));
 
