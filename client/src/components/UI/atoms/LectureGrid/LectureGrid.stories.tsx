f875db5b7cde638d76d0dd459a7c4a97d7dc5eab
jinhyukoo <faill456@naver.com>
2021-02-23 20:37:49 +0900

    feat: LectureGrid 스토리북에 추가

---
+++ client/src/components/UI/atoms/LectureGrid/LectureGrid.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { LectureGrid } from '@/components/UI/atoms';
+
+export default {
+  title: 'atom/LectureGrid',
+  component: LectureGrid,
+  decorators: [withKnobs],
+} as Meta;
+
+const Template = () => <LectureGrid />;
+
+export const testLectureGrid = Template.bind({});
