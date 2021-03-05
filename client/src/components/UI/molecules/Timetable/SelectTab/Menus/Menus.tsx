8ec090b5a3fcad61f8ab244afd5ecd8800731a5a
jinhyukoo <faill456@naver.com>
2021-03-05 15:55:54 +0900

    feat: 시간표 선택탭 메뉴 컴포넌트 제작
    - 생성 버튼
    - 삭제 버튼

---
+++ client/src/components/UI/molecules/Timetable/SelectTab/Menus/Menus.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+import React from 'react';
+import { setModal } from '@/stores/timetable';
+import { Box, Tooltip, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+const Menus = (): JSX.Element => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.buttons}>
+      <Tooltip title="시간표 추가" onClick={() => setModal('ADD')}>
+        <IconButton aria-label="add">
+          <AddCircleTwoToneIcon color="primary" />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="시간표 삭제" onClick={() => setModal('DELETE')}>
+        <IconButton aria-label="delete">
+          <DeleteIcon color="primary" />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
+
+export { Menus };
