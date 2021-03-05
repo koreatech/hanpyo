3299a1c8cb9e0f8a28011ec5d24b0c55dee1d670
jinhyukoo <faill456@naver.com>
2021-03-06 01:25:56 +0900

    feat: Modal Layout, View 설정

--- client/src/components/UI/molecules/Timetable/SelectTab/Menus/Modal.tsx
+++ client/src/components/UI/molecules/Timetable/SelectTab/Menus/Modal.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-closing-bracket-location */
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useReactiveVar } from '@apollo/client';
 import { modalState, modalInfo } from '@/stores/timetable';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { Modal, TextField, Button } from '@material-ui/core';
+import { Typography, TypographyType } from '@/components/UI/atoms';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
@@ -15,10 +16,16 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'center',
     },
     paper: {
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      height: '8rem',
+      backgroundColor: 'white',
+      padding: theme.spacing(4, 7, 4),
+    },
+    button: {
+      width: '25%',
+      marginLeft: 'auto',
     },
   }),
 );
@@ -48,8 +55,11 @@ const TimetableMenuModal = (): JSX.Element => {
         }}>
         <Fade in={nowModalState}>
           <div className={classes.paper}>
+            <Typography size="S" typoType={TypographyType.grey5}>
+              {modalInfo().title}
+            </Typography>
             {modalInfo().isInputRequired && <TextField label={modalInfo().placeHolder} variant="outlined" onChange={onChange} />}
-            <Button color="primary" onClick={() => modalInfo().onSubmit(input)}>
+            <Button className={classes.button} variant="contained" color="primary" onClick={() => modalInfo().onSubmit(input)}>
               {modalInfo().buttonTitle}
             </Button>
           </div>
