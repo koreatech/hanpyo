fd49eb69ca6db4c7f4685fae7874cc26026e66f9
jinhyukoo <faill456@naver.com>
2021-03-15 16:06:45 +0900

    feat: SearchBar 컴포넌트 제작

--- client/src/components/UI/molecules/index.ts
+++ client/src/components/UI/molecules/index.ts
@@ -3,3 +3,4 @@ export { Timetable } from './Timetable/Timetable';
 export type { TimetableProps } from './Timetable/Timetable';
 export { Notice } from './Notice/Notice';
 export { SelectTab } from './Timetable/SelectTab/SelectTab';
+export { SearchBar } from './SearchBar/SearchBar';
