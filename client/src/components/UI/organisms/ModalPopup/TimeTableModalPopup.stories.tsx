e2487aac46cf4341234e064eef7881c5fae3add0
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-03-22 17:18:49 +0900

    feat: TimeTableModalPopup 컴포넌트 제작
    - 시간표 추가/삭제에 대한 TimeTableModalPopup 컴포넌트 제작
    - TimeTableModalPopup 스토리 작성

---
+++ client/src/components/UI/organisms/ModalPopup/TimeTableModalPopup.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+import { TimeTableModalType } from '@/components/UI/molecules';
+import { TimeTableModalPopup, TimeTableModalPopupProps } from './TimeTableModalPopup';
+
+export default {
+  title: 'organisms/TimeTableModalPopup',
+  component: TimeTableModalPopup,
+  decorators: [withKnobs],
+} as Meta;
+
+const Template: Story<TimeTableModalPopupProps> = (args) => <TimeTableModalPopup {...args} />;
+
+export const TabAddModalPopup = Template.bind({});
+TabAddModalPopup.args = {
+  modalOpen: true,
+  modalType: TimeTableModalType.TAB_ADD_MODAL,
+  onModalAreaClose: action('onClick'),
+  onModalBtnClick: action('onClick'),
+};
+
+export const TabRemoveModalPopup = Template.bind({});
+TabRemoveModalPopup.args = {
+  modalOpen: true,
+  modalType: TimeTableModalType.TAB_REMOVE_MODAL,
+  onModalAreaClose: action('onClick'),
+  onModalBtnClick: action('onClick'),
+};
