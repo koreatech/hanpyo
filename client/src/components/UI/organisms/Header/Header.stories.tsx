0ccaa613296fb3ea754f3b0a1bb1611a6362c855
jinhyukoo <faill456@naver.com>
2021-03-24 16:00:30 +0900

    refactor: Header 스토리북 리팩토링
    - 네이밍 변경

--- client/src/components/UI/organisms/Header/Header.stories.tsx
+++ client/src/components/UI/organisms/Header/Header.stories.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { Header } from '@/components/UI/molecules';
+import { Header } from '@/components/UI/organisms';
 
 export default {
-  title: 'molecules/Header',
+  title: 'organisms/Header',
   component: Header,
   decorators: [withKnobs],
 } as Meta;
 
-const Template = () => <Header />;
-
-export const MainHeader = Template.bind({});
+export const header = () => <Header />;
