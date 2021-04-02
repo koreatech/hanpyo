43bb73665725de9c6ac96eea7e5fa987e857c2d8
jinhyukoo <faill456@naver.com>
2021-04-02 16:20:26 +0900

    refactor: LectureInfo Typography 리팩토링
    - 커스텀 Typography 제거

--- client/src/components/UI/atoms/LectureInfoTitle/LectureInfoTitle.tsx
+++ client/src/components/UI/atoms/LectureInfoTitle/LectureInfoTitle.tsx
@@ -1,39 +1,6 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TypographyType } from '@/components/UI/atoms';
-
-const useStyles = makeStyles((theme) => ({
-  default: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  code: {
-    width: '10%',
-  },
-  name: {
-    width: '26%',
-  },
-  class: {
-    width: '5%',
-  },
-  prof: {
-    width: '12%',
-  },
-  grade: {
-    width: '5%',
-  },
-  personnel: {
-    width: '5%',
-  },
-  dept: {
-    width: '21%',
-  },
-  time: {
-    width: '16%',
-  },
-}));
 
 enum LectureInfoTitleType {
   code = 'code',
@@ -52,18 +19,53 @@ interface TitleProps {
   isHeader: boolean;
 }
 
+interface CSSProps {
+  isHeader: boolean;
+}
+
+const useStyles = makeStyles((theme) => ({
+  default: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  code: {
+    width: '10%',
+  },
+  name: {
+    width: '24%',
+  },
+  class: {
+    width: '5%',
+  },
+  prof: {
+    width: '12%',
+  },
+  grade: {
+    width: '5%',
+  },
+  personnel: {
+    width: '5%',
+  },
+  dept: {
+    width: '21%',
+  },
+  time: {
+    width: '18%',
+  },
+  text: {
+    color: (props: CSSProps) => (props.isHeader ? theme.palette.grey[500] : theme.palette.grey[800]),
+  },
+}));
+
 const LectureInfoTitle = ({ className, children, isHeader }: TitleProps): JSX.Element => {
-  const classes = useStyles();
+  const classes = useStyles({ isHeader });
   const getClassName = () => {
     return { ...classes }[className];
   };
-  const getTypoType = () => {
-    if (isHeader) return TypographyType.grey5;
-    return TypographyType.grey8;
-  };
   return (
     <Box className={`${classes.default} ${getClassName()}`}>
-      <Typography size="XS" typoType={getTypoType()}>
+      <Typography className={classes.text} variant="caption">
         {children}
       </Typography>
     </Box>
