2e4a53964621aa91ad04586e16fbbf37c7253151
jinhyukoo <faill456@naver.com>
2021-04-02 16:24:20 +0900

    refactor: Typography 및 안쓰는 파일들 제거
    - 이미 컴포넌트 분리가 끝난 Menus와 Modal 제거
    - Typography 제거

--- client/src/components/UI/atoms/index.ts
+++ client/src/components/UI/atoms/index.ts
@@ -1,7 +1,5 @@
 export { StyledButton as Button, ButtonType } from './Button/Button';
 export type { ButtonProps } from './Button/Button';
-export { StyledTypography as Typography, TypographyType } from './Typography/Typography';
-export type { TypographyProps } from './Typography/Typography';
 export { LectureGrid } from './LectureGrid/LectureGrid';
 export { LectureInfoTitle, LectureInfoTitleType } from './LectureInfoTitle/LectureInfoTitle';
 export { LectureInfoDivider } from './LectureInfoDivider/LectureInfoDivider';
