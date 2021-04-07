091c073bdf229e089138012c87bd4a098a08908d
jinhyukoo <faill456@naver.com>
2021-04-07 19:38:10 +0900

    feat: LoginModalPopup 스토리북 추가

---
+++ client/src/components/UI/organisms/ModalPopup/LoginModalPopup.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+import { LoginModalType } from '@/components/UI/molecules';
+import { LoginModalPopup, LoginModalPopupProps } from './LoginModalPopup';
+
+export default {
+  title: 'organisms/LoginModalPopup',
+  component: LoginModalPopup,
+  decorators: [withKnobs],
+} as Meta;
+
+const Template: Story<LoginModalPopupProps> = (args) => <LoginModalPopup {...args} />;
+
+export const TabAddModalPopup = Template.bind({});
+TabAddModalPopup.args = {
+  modalOpen: true,
+  modalType: LoginModalType.LOGIN_MODAL,
+  onModalAreaClose: action('onClick'),
+  onModalBtnClick: action('onClick'),
+};
