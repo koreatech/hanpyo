0f80742b6488a28d9fce934af4b5af694fe78b35
jinhyukoo <faill456@naver.com>
2021-04-07 19:31:07 +0900

    feat: LoginModalContent 컴포넌트 제작
    - View 관련 작업 완료

--- client/src/components/UI/molecules/LoginModalContent/LoginModalContent.tsx
+++ client/src/components/UI/molecules/LoginModalContent/LoginModalContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, DialogTitle, DialogContent, DialogActions, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 enum LoginModalType {
   LOGIN_MODAL = 'LOGIN_MODAL',
@@ -9,14 +10,32 @@ interface LoginModalContentProps {
   onModalClose: () => void;
 }
 
+const useStyles = makeStyles((theme) => ({
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: '1.7rem',
+    color: theme.palette.primary.main,
+  },
+}));
+
 const LoginModalContent = ({ onModalClose }: LoginModalContentProps): JSX.Element => {
+  const classes = useStyles();
+
   return (
     <>
-      <DialogTitle id="login-dialog-title">한표 로그인</DialogTitle>
+      <DialogTitle className={classes.title} id="login-dialog-title" disableTypography>
+        한표 로그인
+      </DialogTitle>
       <DialogContent>
         <TextField autoFocus margin="dense" id="id" label="아이디" type="email" fullWidth />
-        <TextField autoFocus margin="dense" id="password" label="비밀번호" type="password" fullWidth />
+        <TextField margin="dense" id="password" label="비밀번호" type="password" fullWidth />
       </DialogContent>
+      <DialogActions>
+        <Button onClick={onModalClose} color="primary">
+          로그인
+        </Button>
+      </DialogActions>
     </>
   );
 };
