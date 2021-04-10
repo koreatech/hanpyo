622aaf5e99a326fe5b1d6a7e09ea063b9bad4089
jinhyukoo <faill456@naver.com>
2021-04-10 13:57:54 +0900

    feat: Login 화면 유효성 검사 로직  추가

--- client/src/components/UI/molecules/LoginModalContent/LoginModalContent.tsx
+++ client/src/components/UI/molecules/LoginModalContent/LoginModalContent.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, DialogTitle, DialogContent, DialogActions, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { useDebounce } from '@/common/utils';
 
 enum LoginModalType {
   LOGIN_MODAL = 'LOGIN_MODAL',
@@ -21,6 +22,32 @@ const useStyles = makeStyles((theme) => ({
 
 const LoginModalContent = ({ onModalClose }: LoginModalContentProps): JSX.Element => {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const onEmailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const debouncedEmail = useDebounce({ value: email, delay: 500 });
+  const debouncedPassword = useDebounce({ value: password, delay: 500 });
+
+  useEffect(() => {
+    if (debouncedEmail) {
+      console.log(debouncedEmail.length);
+      if (debouncedEmail.length < 8 || debouncedEmail.length > 12) setIsValidEmail(false);
+      else setIsValidEmail(true);
+    }
+    if (debouncedPassword) {
+      console.log(debouncedPassword.length);
+      if (debouncedPassword.length < 8 || debouncedPassword.length > 12) setIsValidPassword(false);
+      else setIsValidPassword(true);
+    }
+  }, [debouncedEmail, debouncedPassword]);
 
   return (
     <>
@@ -28,8 +55,27 @@ const LoginModalContent = ({ onModalClose }: LoginModalContentProps): JSX.Elemen
         한표 로그인
       </DialogTitle>
       <DialogContent>
-        <TextField autoFocus margin="dense" id="id" label="아이디" type="email" fullWidth />
-        <TextField margin="dense" id="password" label="비밀번호" type="password" fullWidth />
+        <TextField
+          helperText={isValidEmail ? 'koreatech.ac.kr은 빼고 입력해주세요.' : 'Email 형식이 적합하지 않습니다.'}
+          error={!isValidEmail}
+          autoFocus
+          margin="dense"
+          id="id"
+          label="아이디"
+          type="email"
+          fullWidth
+          onChange={onEmailChangeHandler}
+        />
+        <TextField
+          helperText={isValidPassword ? 'Password는 8자 이상 12자 이하로 입력해주세요.' : 'Password 형식이 적합하지 않습니다.'}
+          error={!isValidPassword}
+          margin="dense"
+          id="password"
+          label="비밀번호"
+          type="password"
+          fullWidth
+          onChange={onPasswordChangeHandler}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={onModalClose} color="primary">
