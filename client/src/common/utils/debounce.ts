b223c881a285695e3f4fe86ae7888d4de924c2ec
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-04-22 14:57:13 +0900

    refactor: debounce 유틸 불필요한 eslint 옵션 off
    - debounce 함수 리턴시 argument의 타입을 any를 설정한 no-explicit-any가
      에러로 잡히는 문제 해결

--- client/src/common/utils/debounce.ts
+++ client/src/common/utils/debounce.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-this-alias */
 const debounce = (func: Function, wait: number): (() => void) => {
   let timeout: NodeJS.Timeout | null;
