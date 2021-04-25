5195377b08a73c0316ce6677261e96586d1aebeb
jinhyukoo <faill456@naver.com>
2021-04-25 18:01:41 +0900

    refactor: store의 정보를 가져다 쓰게 수정

--- client/src/components/UI/organisms/LectureReview/LectureReviewContainer.tsx
+++ client/src/components/UI/organisms/LectureReview/LectureReviewContainer.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useReactiveVar } from '@apollo/client';
+import { useStores } from '@/stores';
 import { LectureReview } from './LectureReview';
 
 const useStyles = makeStyles({
@@ -10,59 +12,16 @@ const useStyles = makeStyles({
   },
 });
 
-const mockData = [
-  {
-    infos: {
-      lectureName: '디자인커뮤니케이션',
-      profName: '윤정식',
-      rating: 3.5,
-      period: '2020년도 2학기',
-    },
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam. 안녕하세요 제 이름은 지녀쿠입니다. 한기대학생이 아니지만 한표를 만들고 있답니다 하하',
-    tags: ['꿀수업', '진혀쿠', '돔황챠'],
-    scores: {
-      upScore: 20,
-      downScore: 3,
-    },
-  },
-  {
-    infos: {
-      lectureName: '취준하며놀고먹기',
-      profName: '진혀쿠',
-      rating: 1.5,
-      period: '2021년도 1학기',
-    },
-    content: '취준하면서 놀고 먹고 싶다구요? 저처럼 공부를 안 하면 된답니다!',
-    tags: ['백수', '진혀쿠', '나처럼살지마'],
-    scores: {
-      upScore: 5,
-      downScore: 18,
-    },
-    isMine: true,
-  },
-  {
-    infos: {
-      lectureName: '디자인커뮤니케이션',
-      profName: '윤정식',
-      rating: 4,
-      period: '2020년도 2학기',
-    },
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam. 안녕하세요 제 이름은 지녀쿠입니다. 한기대학생이 아니지만 한표를 만들고 있답니다 하하',
-    tags: ['꿀수업', '진혀쿠', '돔황챠'],
-    scores: {
-      upScore: 20,
-      downScore: 3,
-    },
-  },
-];
-
 const LectureReviewContainer = (): JSX.Element => {
   const classes = useStyles();
+  const { lectureReviewStore } = useStores();
+  const reviews = useReactiveVar(lectureReviewStore.state.reviews);
+
   const getLectureReviews = () => {
-    return mockData.map((data, idx) => {
-      return <LectureReview key={idx} infos={data.infos} content={data.content} tags={data.tags} scores={data.scores} isMine={data.isMine} />;
+    return reviews.map((review, idx) => {
+      return (
+        <LectureReview key={idx} infos={review.infos} content={review.content} tags={review.tags} scores={review.scores} isMine={review.isMine} />
+      );
     });
   };
   return <div className={classes.root}>{getLectureReviews()}</div>;
