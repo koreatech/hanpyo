eb49decc26551cd851703d6dd83cfa85daf961a3
jinhyukoo <faill456@naver.com>
2021-04-27 21:30:27 +0900

    modify: props 변경에 따른 storybook 코드 수정

--- client/src/components/UI/organisms/LectureReview/LectureReview.stories.tsx
+++ client/src/components/UI/organisms/LectureReview/LectureReview.stories.tsx
@@ -10,6 +10,23 @@ export default {
   decorators: [withKnobs],
 } as Meta;
 
+const mockData = {
+  id: 0,
+  infos: {
+    lectureName: '디자인커뮤니케이션',
+    profName: '윤정식',
+    rating: 3.5,
+    period: '2020년도 2학기',
+  },
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam. 안녕하세요 제 이름은 지녀쿠입니다. 한기대학생이 아니지만 한표를 만들고 있답니다 하하',
+  tags: ['꿀수업', '진혀쿠', '돔황챠', '진혀쿠', '돔황챠', '진혀쿠', '돔황챠', '진혀쿠', '돔황챠', '진혀쿠', '돔황챠', '진혀쿠', '돔황챠'],
+  scores: {
+    upScore: 20,
+    downScore: 3,
+  },
+};
+
 const Template: Story<LectureReviewProps> = (args) => {
   const LectureReviewStory = withStoryBox(args, 800)(LectureReview);
   return <LectureReviewStory {...args} />;
@@ -17,33 +34,11 @@ const Template: Story<LectureReviewProps> = (args) => {
 
 export const MyReview = Template.bind({});
 MyReview.args = {
-  infos: {
-    lectureName: '취준하며놀고먹기',
-    profName: '진혀쿠',
-    rating: 1.5,
-    period: '2021년도 1학기',
-  },
-  content: '취준하면서 놀고 먹고 싶다구요? 저처럼 공부를 안 하면 된답니다!',
-  tags: ['백수', '진혀쿠', '나처럼살지마'],
-  scores: {
-    upScore: 5,
-    downScore: 18,
-  },
+  data: mockData,
   isMine: true,
 };
 
 export const OthersReview = Template.bind({});
 OthersReview.args = {
-  infos: {
-    lectureName: '멋지게취업하기',
-    profName: '갓우진',
-    rating: 5,
-    period: '2021년도 1학기',
-  },
-  content: '나는야 갓우진 그린팩토리를 점령할 사람이지!',
-  tags: ['취뽀', '신과함께'],
-  scores: {
-    upScore: 1000,
-    downScore: 0,
-  },
+  data: mockData,
 };
