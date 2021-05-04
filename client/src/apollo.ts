943b3c91db599bbfd559c01a437803c8eee35e04
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-05-04 20:51:04 +0900

    refactor: ApolloClient 불필요한 코드 제거 및 CORS 자격증명 옵션 추가
    - CORS 요청시 쿠키 전송을 위한 credentials 옵션 추가

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,35 +1,13 @@
-import { ApolloClient, HttpLink, split, InMemoryCache } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { OperationDefinitionNode } from 'graphql';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-// graphql api 주소
-const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+const link = new HttpLink({
+  uri: '/graphql',
+  credentials: 'include',
 });
 
-// // subscription socket 통신 주소
-// const wsLink = new WebSocketLink({
-//   uri: `ws://localhost:4000/graphql`,
-//   options: {
-//     reconnect: true,
-//   },
-// });
-
-// 작성한 두 주소 병합
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query) as OperationDefinitionNode;
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  // wsLink,
-  httpLink,
-);
-
-// apollo client 생성
 const client = new ApolloClient({
   link,
-  cache: new InMemoryCache({}),
+  cache: new InMemoryCache(),
 });
 
 export default client;
