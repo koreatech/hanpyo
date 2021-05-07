81daa66087e57d68debad856a6d8cd7bea4b84f3
jinhyukoo <faill456@naver.com>
2021-05-07 17:18:02 +0900

    feat: LectureList에 전달될 로직을 위한 컴포넌트 제작
    - 기존에 LectureList에 담겨있던 로직을 컴포넌트 종류에 따라 구분하여 분리

---
+++ client/src/components/UI/organisms/LectureList/SearchedLectureList.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { SnackbarType } from '@/components/UI/atoms';
+import { LectureInfos, LectureListContent } from '@/components/UI/molecules';
+import { useStores } from '@/stores';
+import { isString } from '@/common/utils/typeCheck';
+import { useQuery, gql } from '@apollo/client';
+
+const LECTURE_INFOS = gql`
+  query GetLectureInfos {
+    lectureInfos {
+      id
+      code
+      name
+      department
+      room
+      professor
+      requiredGrade
+      requiredMajor
+      divisionNumber
+      department
+      lectureTimes {
+        start
+        end
+      }
+    }
+  }
+`;
+
+const SearchedLectureList = () => {
+  const { timeTableStore, snackbarStore, lectureInfoStore } = useStores();
+
+  const onLectureSearchDoubleClickListener = (lectureInfos: LectureInfos) => {
+    if (isString(lectureInfos.lectureTimes)) return;
+
+    timeTableStore.addLectureToTable(lectureInfos);
+    snackbarStore.setSnackbarType(SnackbarType.ADD_SUCCESS);
+    snackbarStore.setSnackbarState(true);
+  };
+
+  const onLectureSearchClickListener = (lectureInfos: LectureInfos) => {
+    if (isString(lectureInfos.lectureTimes)) return;
+
+    lectureInfoStore.state.selectedLecture(lectureInfos);
+  };
+
+  const { loading, error, data } = useQuery(LECTURE_INFOS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data.lectureInfos.slice(0, 20));
+
+  return (
+    <LectureListContent
+      onClick={onLectureSearchClickListener}
+      onDoubleClick={onLectureSearchDoubleClickListener}
+      lectureInfos={data.lectureInfos.slice(500, 600)}
+    />
+  );
+};
+
+export { SearchedLectureList };
