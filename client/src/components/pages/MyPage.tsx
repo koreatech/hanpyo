8c89c16151bf684d5c2420af6fa1d804ce622337
jinhyukoo <faill456@naver.com>
2021-05-09 17:37:07 +0900

    feat: 마이페이지 레이아웃 작업 완료
    - header, body, bodyleft, bodyright로 구성

--- client/src/components/pages/MyPage.tsx
+++ client/src/components/pages/MyPage.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '1000px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-});
+  header: {
+    display: 'flex',
+    width: '70rem',
+    padding: '1rem 0',
+  },
+  body: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '70rem',
+    height: '40rem',
+  },
+  left: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    width: '32.5%',
+    padding: '1.5rem',
+    border: `1px solid ${theme.palette.grey[300]}`,
+    borderRadius: '1rem',
+  },
+  right: {
+    display: 'flex',
+    justifyContent: 'center',
+    boxSizing: 'border-box',
+    padding: '1.5rem',
+    width: '62.5%',
+    border: `1px solid ${theme.palette.grey[300]}`,
+    borderRadius: '1rem',
+  },
+}));
 
 const MyPage = (): JSX.Element => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <div>마이페이지</div>
+      <div className={classes.header}>마이페이지 헤더</div>
+      <div className={classes.body}>
+        <div className={classes.left}>바디왼쪽</div>
+        <div className={classes.right}>바디오른쪽</div>
+      </div>
     </div>
   );
 };
