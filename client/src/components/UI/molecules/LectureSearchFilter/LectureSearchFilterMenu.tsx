23e71733e5ed394f1f7f239e0cb20771d0b34811
jinhyukoo <faill456@naver.com>
2021-05-14 01:46:17 +0900

    chore: 개설학부 추가 및 이름 수정

--- client/src/components/UI/molecules/LectureSearchFilter/LectureSearchFilterMenu.tsx
+++ client/src/components/UI/molecules/LectureSearchFilter/LectureSearchFilterMenu.tsx
@@ -9,12 +9,14 @@ const LectureSearchFilterMenu = (): JSX.Element => {
     menuLabel: '개설학부',
     menus: [
       { id: 0, title: '컴퓨터공학부', value: 0 },
-      { id: 1, title: '디자인공학부', value: 1 },
+      { id: 1, title: '디자인ㆍ건축공학부', value: 1 },
       { id: 2, title: '기계공학부', value: 2 },
       { id: 3, title: '메카트로닉스공학부', value: 3 },
-      { id: 4, title: '전기전자통신공학부', value: 4 },
+      { id: 4, title: '전기ㆍ전자ㆍ통신공학부', value: 4 },
       { id: 5, title: '에너지신소재화학공학부', value: 5 },
       { id: 6, title: '산업경영학부', value: 6 },
+      { id: 7, title: '교양학부', value: 7 },
+      { id: 8, title: 'HRD학과', value: 8 },
     ],
     onSelectMenuChange: (value: string) => {
       lectureInfoStore.state.selectedDepartment(value);
