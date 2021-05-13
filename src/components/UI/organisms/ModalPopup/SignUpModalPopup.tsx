5f867952a7d08f60f12d060937c4501341441fdd
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-05-13 22:08:54 +0900

    refactor: 회원가입 폼 코드 리팩토링
    - useInputForm Hook을 활용하여 input 상태와 관련된 비즈니스 로직 코드
      리팩토링

--- src/components/UI/organisms/ModalPopup/SignUpModalPopup.tsx
+++ src/components/UI/organisms/ModalPopup/SignUpModalPopup.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { SnackbarType } from '@/components/UI/atoms';
 import { ModalPopupArea, SignUpModalContent } from '@/components/UI/molecules';
 import { modalTypes } from '@/components/UI/organisms';
-import { isEmailID, isPassword, isName, isNickname, isGrade, isMajor } from '@/common/utils/validator';
-import { debounce } from '@/common/utils';
 import { useMutation } from '@apollo/client';
 import { SIGN_UP } from '@/queries';
 import { useStores } from '@/stores';
+import { useInputForm } from '@/common/hooks';
 
 interface SignUpModalPopupProps {
   modalOpen: boolean;
   onModalAreaClose: () => void;
+}
+
+interface InputState {
+  email: string;
+  password: string;
+  name: string;
+  nickname: string;
+  grade: string;
+  major: string;
 }
 
 const INIT_INPUTS_STATE = {
@@ -22,21 +30,11 @@ const INIT_INPUTS_STATE = {
   major: '',
 };
 
-const INIT_VALID_STATE = {
-  isValidEmail: true,
-  isValidPassword: true,
-  isValidName: true,
-  isValidNickname: true,
-  isValidGrade: true,
-  isValidMajor: true,
-};
-
 const SignUpModalPopup = ({ modalOpen, onModalAreaClose }: SignUpModalPopupProps): JSX.Element => {
-  const [inputs, setInputs] = useState(INIT_INPUTS_STATE);
+  const [inputs, onInputChange, { reset, isEmpty, valids, isValid }] = useInputForm<InputState>(INIT_INPUTS_STATE, {
+    validation: true,
+  });
   const { email, password, name, nickname, grade, major } = inputs;
-
-  const [valids, setValids] = useState(INIT_VALID_STATE);
-  const { isValidEmail, isValidPassword, isValidName, isValidNickname, isValidGrade, isValidMajor } = valids;
 
   const { modalStore, snackbarStore } = useStores();
   const [signup] = useMutation(SIGN_UP, {
@@ -52,48 +50,6 @@ const SignUpModalPopup = ({ modalOpen, onModalAreaClose }: SignUpModalPopupProps
     },
   });
 
-  const onDebouncedEmailChangeListener = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: emailIDValue } = e.target;
-
-    setInputs({ ...inputs, email: emailIDValue });
-    setValids({ ...valids, isValidEmail: isEmailID(emailIDValue) });
-  }, 500);
-
-  const onDebouncedPasswordChangeListener = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: passwordValue } = e.target;
-
-    setInputs({ ...inputs, password: passwordValue });
-    setValids({ ...valids, isValidPassword: isPassword(passwordValue) });
-  }, 500);
-
-  const onDebouncedNameChangeListener = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: nameValue } = e.target;
-
-    setInputs({ ...inputs, name: nameValue });
-    setValids({ ...valids, isValidName: isName(nameValue) });
-  }, 500);
-
-  const onNicknameChangeListener = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: nickNameValue } = e.target;
-
-    setInputs({ ...inputs, nickname: nickNameValue });
-    setValids({ ...valids, isValidNickname: isNickname(nickNameValue) });
-  }, 500);
-
-  const onGradeChangeListener = debounce((event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: gradeValue } = event.target;
-
-    setInputs({ ...inputs, grade: gradeValue });
-    setValids({ ...valids, isValidGrade: isGrade(gradeValue) });
-  }, 500);
-
-  const onMajorChangeListener = debounce((event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value: majorValue } = event.target;
-
-    setInputs({ ...inputs, major: majorValue });
-    setValids({ ...valids, isValidMajor: isMajor(majorValue) });
-  }, 500);
-
   const onMoveLoginBtnClickListener = () => {
     modalStore.changeModalState(modalTypes.LOGIN_MODAL, true);
   };
@@ -104,52 +60,23 @@ const SignUpModalPopup = ({ modalOpen, onModalAreaClose }: SignUpModalPopupProps
     });
   };
 
-  const resetFormDatas = () => {
-    setInputs(INIT_INPUTS_STATE);
-  };
-
-  const resetValidDatas = () => {
-    setValids(INIT_VALID_STATE);
-  };
-
-  const resetSignupForm = () => {
-    resetFormDatas();
-    resetValidDatas();
-  };
-
   const onSignupModalCloseListener = () => {
-    resetSignupForm();
+    reset();
     onModalAreaClose();
   };
 
-  const checkEmptyFormDatas = (): boolean => {
-    return !(email && password && name && nickname && grade && major);
-  };
-
-  const checkIsValidDatas = (): boolean => {
-    return isValidEmail && isValidPassword && isValidName && isValidNickname && isValidGrade && isValidMajor;
-  };
-
   const checkSignupDisabled = (): boolean => {
-    const isExistEmptyFormDatas = checkEmptyFormDatas();
-    const isValidDatas = checkIsValidDatas();
-
-    return !(!isExistEmptyFormDatas && isValidDatas);
+    return !(!isEmpty && isValid);
   };
 
   return (
     <ModalPopupArea modalOpen={modalOpen} onModalClose={onSignupModalCloseListener}>
       <SignUpModalContent
-        valid={{ isValidEmail, isValidPassword, isValidName, isValidNickname, isValidGrade, isValidMajor }}
+        valid={valids}
         selectValue={{ gradeValue: grade, majorValue: major }}
         isSignupDisabled={checkSignupDisabled()}
+        onInputChange={onInputChange}
         onSignupBtnClick={onSignUpBtnClickListener}
-        onEmailChange={onDebouncedEmailChangeListener}
-        onPasswordChange={onDebouncedPasswordChangeListener}
-        onNameChange={onDebouncedNameChangeListener}
-        onNicknameChange={onNicknameChangeListener}
-        onGradeChange={onGradeChangeListener}
-        onMajorChange={onMajorChangeListener}
         onMoveLoginBtnClick={onMoveLoginBtnClickListener}
       />
     </ModalPopupArea>
