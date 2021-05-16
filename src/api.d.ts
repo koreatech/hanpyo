ba92ba5ea89cad4b108153b51d3ca7329b7a2ade
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-05-17 02:01:47 +0900

    chore: graphQL 스키마 타입 추가
    - myMemberInfo에 대한 graphQL 스키마 타입 추가

--- src/api.d.ts
+++ src/api.d.ts
@@ -4,6 +4,60 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL query operation: GetLectureInfos
+// ====================================================
+
+export interface GetLectureInfos_lectureInfos_lectureTimes {
+  __typename: "LectureTime";
+  start: number;
+  end: number;
+}
+
+export interface GetLectureInfos_lectureInfos {
+  __typename: "Lecture";
+  id: string;
+  code: string;
+  name: string;
+  department: string;
+  room: string | null;
+  professor: string | null;
+  credit: number;
+  requiredGrade: number | null;
+  requiredMajor: string | null;
+  divisionNumber: number;
+  totalStudentNumber: number;
+  lectureTimes: (GetLectureInfos_lectureInfos_lectureTimes | null)[] | null;
+}
+
+export interface GetLectureInfos {
+  lectureInfos: (GetLectureInfos_lectureInfos | null)[];
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetMyMemberInfo
+// ====================================================
+
+export interface GetMyMemberInfo_myMemberInfo {
+  __typename: "Member";
+  nickname: string | null;
+  major: string | null;
+}
+
+export interface GetMyMemberInfo {
+  myMemberInfo: GetMyMemberInfo_myMemberInfo;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: SignUp
 // ====================================================
 
@@ -20,6 +74,15 @@ export interface SignUp_signUp {
 
 export interface SignUp {
   signUp: SignUp_signUp;
+}
+
+export interface SignUpVariables {
+  email: string;
+  password: string;
+  name: string;
+  nickname: string;
+  grade: number;
+  major: string;
 }
 
 /* tslint:disable */
