1332d1baa4276b553294bc990488b43531e1f7a8
jinhyukoo <faill456@naver.com>
2021-05-19 22:49:44 +0900

    feat: 경고 타입 및 메시지 추가

--- src/components/UI/atoms/AlertSnackbar/AlertSnackbar.tsx
+++ src/components/UI/atoms/AlertSnackbar/AlertSnackbar.tsx
@@ -13,6 +13,10 @@ enum SnackbarType {
   LOGIN_SUCCESS = 'LOGIN_SUCCESS',
   LOGIN_FAILED = 'LOGIN_FAILED',
   MY_SCHEDULE_ADD = 'MY_SCHEDULE_ADD',
+  DUPLICATE_LECTURE_NAME = 'DUPLICATE_LECTURE_NAME',
+  DUPLICATE_LECTURE_TIME = 'DUPLICATE_LECTURE_TIME',
+  INVALID_TIME = 'INVALID_TIME',
+  NO_TIMETABLE = 'NO_TIMETABLE',
 }
 
 const SNACKBAR_MESSAGE = {
@@ -23,6 +27,10 @@ const SNACKBAR_MESSAGE = {
   [SnackbarType.LOGIN_SUCCESS]: '정상적으로 로그인되었습니다.',
   [SnackbarType.LOGIN_FAILED]: '로그인이 실패하였습니다. 다시 시도해주세요.',
   [SnackbarType.MY_SCHEDULE_ADD]: '나만의 시간표가 추가되었습니다.',
+  [SnackbarType.DUPLICATE_LECTURE_NAME]: '추가하려는 과목과 중복되는 과목이 있습니다.',
+  [SnackbarType.DUPLICATE_LECTURE_TIME]: '추가하려는 과목과 중복되는 시간이 있습니다.',
+  [SnackbarType.INVALID_TIME]: '시간이 유효하지 않습니다.',
+  [SnackbarType.NO_TIMETABLE]: '시간표를 만든 후 과목을 추가해주세요.',
 };
 
 const AlertSnackbar = (): JSX.Element => {
