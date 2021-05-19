8df09e4b38b51f6797562d8de921ff644cebd639
jinhyukoo <faill456@naver.com>
2021-05-19 21:39:14 +0900

    feat: 나만의 시간표 추가 로직 작성

--- src/components/UI/molecules/TimeTableAddForm/TimeTableAddForm.tsx
+++ src/components/UI/molecules/TimeTableAddForm/TimeTableAddForm.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { getTimeBoundByDay } from '@/common/utils';
+import { useStores } from '@/stores';
 import { TimeTableAddFormContent } from './TimeTableAddFormContent';
 
 const TimeTableAddForm = (): JSX.Element => {
   const [dayState, setDayState] = useState('');
   const [startTimeState, setStartTimeState] = useState(false);
   const [endTimeState, setEndTimeState] = useState(false);
+  const [startTimeValueState, setStartTimeValueState] = useState(0);
+  const [endTimeValueState, setEndTimeValueState] = useState(0);
+  const [inputState, setInputState] = useState('');
+
+  const { timeTableStore } = useStores();
 
   const daySelectMenuProps = {
     state: dayState,
@@ -23,8 +30,9 @@ const TimeTableAddForm = (): JSX.Element => {
   };
 
   const startTimeSelectMenuProps = {
-    state: startTimeState,
-    setState: setStartTimeState,
+    selected: startTimeState,
+    setSelected: setStartTimeState,
+    setTimeValue: setStartTimeValueState,
     menuLabel: '시간',
     onSelectMenuChange: () => {
       console.log('시간선택');
@@ -32,8 +40,9 @@ const TimeTableAddForm = (): JSX.Element => {
   };
 
   const endTimeSelectMenuProps = {
-    state: endTimeState,
-    setState: setEndTimeState,
+    selected: endTimeState,
+    setSelected: setEndTimeState,
+    setTimeValue: setEndTimeValueState,
     menuLabel: '시간',
     onSelectMenuChange: () => {
       console.log('시간선택');
@@ -43,12 +52,41 @@ const TimeTableAddForm = (): JSX.Element => {
     alert('submit!');
   };
 
+  const onButtonClickListener = () => {
+    if (!dayState && !startTimeState && !endTimeState && inputState) return;
+    const dayBound = getTimeBoundByDay(dayState);
+
+    const newCustomLecture = {
+      id: 0,
+      code: '',
+      name: inputState,
+      divisionNumber: ' ',
+      professor: '',
+      totalStudentNumber: '',
+      department: '나만의 스케줄',
+      lectureTimes: [{ start: startTimeValueState + dayBound.start, end: endTimeValueState + dayBound.start }],
+      room: '',
+      requiredGrade: '',
+      requiredMajor: '',
+      credit: '',
+    };
+
+    console.log(newCustomLecture);
+    timeTableStore.addLectureToTable(newCustomLecture);
+  };
+
+  const onInputChangeListener = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputState(event.target.value);
+  };
   return (
     <TimeTableAddFormContent
       daySelectMenu={daySelectMenuProps}
       startTimeSelectMenu={startTimeSelectMenuProps}
       endTimeSelectMenu={endTimeSelectMenuProps}
       onTimeTableFormSubmit={onTimeTableFormSubmitListener}
+      inputState={inputState}
+      onInputChange={onInputChangeListener}
+      onBtnClick={onButtonClickListener}
     />
   );
 };
