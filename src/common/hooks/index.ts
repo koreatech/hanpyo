5cefa0d9ec7d0e62dc46991537bf348976b2e9e4
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-05-22 20:07:08 +0900

    feat: useReactiveVars hook 제작
    - useReactiveVar를 이용하여 상태변수를 여러개 생성할 경우, 활용할 수 있도록 useReactiveVars hook 제작
    - ReactiveVar 타입 프로퍼티들을 가진  state를  useReactiveVar가 적용된 객체로 리턴해도록 구현

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useFetchAsync } from './useFetchAsync';
 export { default as useInputForm } from './useInputForm';
+export { default as useReactiveVars } from './useReactiveVars';
