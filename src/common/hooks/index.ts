8f504ce1d88dc928fba1b96a1635e5dda3f7c03b
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2021-05-23 01:58:24 +0900

    feat: useTimeSelect hook 제작
    - TimeSelectMenu 비즈니스 로직 재사용을 위한 hook 제작

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useFetchAsync } from './useFetchAsync';
 export { default as useInputForm } from './useInputForm';
 export { default as useReactiveVars } from './useReactiveVars';
+export { default as useTimeSelect } from './useTimeSelect';
---
+++ src/common/hooks/useTimeSelect.ts
@@ -0,0 +1,113 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useReducer, useMemo } from 'react';
+
+interface TimeSelectState {
+  selectedAMPM: string | undefined;
+  selectedHour: string | undefined;
+  selectedMinute: string | undefined;
+  isSelect: boolean;
+}
+
+enum TimeSelectValueType {
+  AM_PM = 'AM_PM',
+  HOUR = 'HOUR',
+  MINUTE = 'MINUTE',
+}
+
+enum TimeSelectActionType {
+  SELECT_AM_PM = 'SELECT_AM_PM',
+  SELECT_HOUR = 'SELECT_HOUR',
+  SELECT_MINUTE = 'SELECT_MINUTE',
+  RESET = 'RESET',
+}
+
+interface TimeSelectAction {
+  type: TimeSelectActionType;
+  value?: string;
+}
+
+interface Option {
+  callback: Function;
+}
+
+interface UseTimeSelectArgs {
+  option?: Option;
+}
+
+const INIT_STATE: TimeSelectState = {
+  selectedAMPM: '오전',
+  selectedHour: '01',
+  selectedMinute: '00',
+  isSelect: false,
+};
+
+function reducer(prevState: TimeSelectState, action: TimeSelectAction): TimeSelectState {
+  switch (action.type) {
+    case TimeSelectActionType.SELECT_AM_PM:
+      return { ...prevState, selectedAMPM: action.value, isSelect: true };
+    case TimeSelectActionType.SELECT_HOUR:
+      return { ...prevState, selectedHour: action.value, isSelect: true };
+    case TimeSelectActionType.SELECT_MINUTE:
+      return { ...prevState, selectedMinute: action.value, isSelect: true };
+    case TimeSelectActionType.RESET:
+      return INIT_STATE;
+    default:
+      return prevState;
+  }
+}
+
+function useTimeSelect({
+  option,
+}: UseTimeSelectArgs): [string, number, boolean, (e: React.MouseEvent<HTMLLIElement>) => void, (itemValue: string) => boolean, () => void] {
+  const [state, dispatch] = useReducer<React.Reducer<TimeSelectState, TimeSelectAction>, TimeSelectState>(reducer, INIT_STATE, () => INIT_STATE);
+  const { selectedAMPM, selectedHour, selectedMinute, isSelect } = state;
+
+  const value = `${selectedAMPM}${selectedHour && `  ${selectedHour} : `}${selectedMinute}`;
+
+  const calculateTime = (): number => {
+    const ampm = selectedAMPM;
+    const hour = Number(selectedHour);
+    const minute = Number(selectedMinute);
+
+    let time = ampm === '오후' ? 720 : 0;
+    time += hour * 60 + minute;
+
+    return time;
+  };
+
+  const time = useMemo(calculateTime, [state]);
+
+  const onMenuClick = (e: React.MouseEvent<HTMLLIElement>) => {
+    const { dataset } = (e.target as HTMLElement).closest('li') as HTMLLIElement;
+    const { type, title, selectType } = dataset;
+
+    if (type === TimeSelectValueType.AM_PM) {
+      dispatch({ type: TimeSelectActionType.SELECT_AM_PM, value: title ?? '' });
+    }
+
+    if (type === TimeSelectValueType.HOUR) {
+      dispatch({ type: TimeSelectActionType.SELECT_HOUR, value: title ?? '' });
+    }
+
+    if (type === TimeSelectValueType.MINUTE) {
+      dispatch({ type: TimeSelectActionType.SELECT_MINUTE, value: title ?? '' });
+    }
+
+    if (option?.callback) {
+      option.callback(selectType, time);
+    }
+  };
+
+  const checkSelectedItem = (itemValue: string): boolean => {
+    return itemValue === selectedAMPM || itemValue === selectedHour || itemValue === selectedMinute;
+  };
+
+  const resetTimeSelectState = () => {
+    dispatch({ type: TimeSelectActionType.RESET });
+  };
+
+  return [value, time, isSelect, onMenuClick, checkSelectedItem, resetTimeSelectState];
+}
+
+export default useTimeSelect;
