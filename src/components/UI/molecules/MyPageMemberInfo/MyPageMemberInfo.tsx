d9e77b1fcc7ebb14e01088403537f219f8601506
jinhyukoo <faill456@naver.com>
2021-06-08 14:26:23 +0900

    feat: MyPage 회원 정보 레이아웃 및 이미지 작업

---
+++ src/components/UI/molecules/MyPageMemberInfo/MyPageMemberInfo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Avatar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { MY_MEMBER_INFO } from '@/queries';
+import client from '@/apollo';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+  },
+  image: {
+    width: '12rem',
+    height: '12rem',
+  },
+}));
+
+const MyPageMemberInfo = (): JSX.Element => {
+  const classes = useStyles();
+
+  const { myMemberInfo } = client.readQuery({
+    query: MY_MEMBER_INFO,
+  });
+
+  console.log(myMemberInfo);
+
+  return (
+    <div className={classes.root}>
+      <Avatar alt="profile img" className={classes.image} />
+    </div>
+  );
+};
+
+export { MyPageMemberInfo };
