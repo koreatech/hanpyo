ebc908196f01e7a098237e64aee42d5ad02285c0
jinhyukoo <faill456@naver.com>
2021-06-08 14:38:57 +0900

    feat: MyPageContentViewer에 MyPageMemberInfo 추가

--- src/components/UI/organisms/MyPageContentViewer/MyPageContentViewer.tsx
+++ src/components/UI/organisms/MyPageContentViewer/MyPageContentViewer.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useReactiveVar } from '@apollo/client';
 import { useStores } from '@/stores';
 import { MyPageMenuType } from '@/components/UI/atoms';
+import { MyPageMemberInfo } from '@/components/UI/molecules';
 
 const MyPageContentViewer = (): JSX.Element => {
   const { myPageStore } = useStores();
   const selectedMenuType = useReactiveVar(myPageStore.state.menuType);
 
-  if (selectedMenuType === MyPageMenuType.MEMBER_INFO) return <>회원정보</>;
+  if (selectedMenuType === MyPageMenuType.MEMBER_INFO) return <MyPageMemberInfo />;
   if (selectedMenuType === MyPageMenuType.CHANGE_PASSWORD) return <>비밀번호 변경</>;
   if (selectedMenuType === MyPageMenuType.MY_POSTING) return <>내 게시물</>;
   if (selectedMenuType === MyPageMenuType.FRIEND_MANAGEMENT) return <>친구 관리</>;
